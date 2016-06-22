a59067d84201a9cf71feb357ffd46bbffd030610
Petr Tsurinov <petr_tsurinov@epam.com>
2016-06-22 15:52:44 +0300

    moved transactional annotation

--- src/main/java/com/epam/pts/training/service/DogService.java
+++ src/main/java/com/epam/pts/training/service/DogService.java
@@ -2,12 +2,14 @@ package com.epam.pts.training.service;
 
 import com.epam.pts.training.dao.DogDao;
 import com.epam.pts.training.entity.Dog;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
 public class DogService {
     private DogDao dogDao;
 
+    @Transactional
     public Collection<Dog> getDogs() {
         return dogDao.getDogs();
     }
